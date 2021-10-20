1218656547755d784d16cf34078358b478a52b1f
AzazyroEquihua <A01633438@itesm.mx>
2021-10-19 20:52:48 -0500

    Prueba

---
+++ Regla_1.m
@@ -0,0 +1,23 @@
+function output_image = Regla_1(input_image)
+
+[fil, col, channel] = size(input_image);
+output_image = double(zeros(fil, col));
+
+for x = 1:fil
+    for y = 1:col
+        if (input_image(x, y, 2) > input_image(x, y, 3))
+            if (input_image(x, y, 1) > input_image(x, y, 2))
+                output_image(x, y) = 1;
+            end
+        else
+            output_image(x, y) = 0;
+        end
+    end
+end
+
+% figure('Name','Imagen obtenida mediante regla 1','NumberTitle','off');
+% imshow(output_image)
+output_image = uint8(output_image);
+
+
+end
---
+++ Regla_2.m
@@ -0,0 +1,20 @@
+function output_image = Regla_2(input_image)
+
+[fil, col, channel] = size(input_image);
+output_image = double(zeros(fil, col));
+
+for x = 1:fil
+    for y = 1:col
+        if input_image(x, y, 1) > 190  &&  input_image(x, y, 2) > 100  &&  input_image(x, y, 3) < 140
+            output_image(x, y) = 1;
+        else
+            output_image(x, y) = 0;
+        end
+    end
+end
+
+% figure('Name','Imagen obtenida mediante regla 2','NumberTitle','off');
+% imshow(output_image)
+output_image = uint8(output_image);
+
+end
