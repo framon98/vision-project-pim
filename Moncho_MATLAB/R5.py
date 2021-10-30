9d7e8e32c07bc36de48aabf8098c75bc0c052581
Francisco R Castro <iceman09@hotmail.com>
2021-10-30 14:56:49 -0500

    Added avg to the channels, comments for testing split, uint conversion of avgs

--- Moncho_MATLAB/R5.py
+++ Moncho_MATLAB/R5.py
@@ -8,13 +8,55 @@ def R5(image):
     im_read = cv2.imread(image)
 
     fil, col, ch = im_read.shape
-    fil = int(fil)
-    col = int(col)
-    ch = int(ch)
 
-    im_YCbCr = cv2.cvtColor(im_read, cv2.COLOR_RGB2YCrCb)
+    im_YCbCr = cv2.cvtColor(im_read, cv2.COLOR_BGR2YCrCb)
+    print(im_YCbCr.shape)
     im_filtro = np.zeros((fil, col))
 
-    im_Y = im_YCbCr[:][:][0]
-    im_Cb = im_YCbCr[:][:][1]
-    im_Cr = im_YCbCr[:][:][2]
+    # im_Y = np.zeros((fil, col))
+    # im_Cb = np.zeros((fil, col))
+    # im_Cr = np.zeros((fil, col))
+
+    Ysuma = 0
+    Cbsuma = 0
+    Crsuma = 0
+    MxN = fil*col
+
+    
+    (im_Y, im_Cb, im_Cr) = cv2.split(im_YCbCr) #, [im_Y, im_Cb, im_Cr]
+
+    # im_Y = im_YCbCr[:][:][0]
+    # im_Cb = im_YCbCr[:][:][1]
+    # im_Cr = im_YCbCr[:][:][2]
+
+    # print(im_Y.shape)
+    # cv2.imshow("Y channel", im_Y)
+    # print(im_Cb.shape)
+    # cv2.imshow("Cb channel", im_Cb)
+    # print(im_Cr.shape)
+    # cv2.imshow("Cr channel", im_Cr)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    # Promedios de cada canal
+    for idx in range(0, fil):
+        for idy in range(0, col):
+            Ysuma = im_Y[idx][idy] + Ysuma
+            Cbsuma = im_Cb[idx][idy] + Cbsuma
+            Crsuma = im_Cr[idx][idy] + Crsuma
+
+    Yprom = (Ysuma/MxN)
+    Cbprom = (Cbsuma/MxN)
+    Crprom = (Crsuma/MxN)
+
+    Yprom = np.uint8(Yprom)
+    Cbprom = np.uint8(Cbprom)
+    Crprom = np.uint8(Crprom)
+
+    print(Yprom)
+    print(Cbprom)
+    print(Crprom)
+
+    # cv2.imshow("Rule 3", im_filtro)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
